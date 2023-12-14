8a5673aab6971d1d8ca949c05fc6ae87afcca1aa
Marek Załęcki <mzalecki.ignis@gmail.com>
2023-12-14 19:32:09 +0100

    status

--- main.py
+++ main.py
@@ -1,16 +1,87 @@
-# This is a sample Python script.
+import argparse
+import datetime
+from pathlib import Path
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import mlflow
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+
+from src.data.data import DiceImageDataset
+from src.training.classifier import train_loop, test_loop
+from src.models.simple_cnn import SimpleCNN
+
+# -------- parse arguments --------
+parser = argparse.ArgumentParser(description='Train a classifier')
+parser.add_argument('--model', type=str, help='Path to model file', required=True)
+parser.add_argument('--data', type=str, help='Path to data directory', required=True)
+
+parser.add_argument('--epochs', type=int, help='Number of epochs', default=10)
+parser.add_argument('--batch_size', type=int, help='Batch size', default=32)
+parser.add_argument('--learning_rate', type=float, help='Learning rate', default=1e-3)
+
+parser.add_argument('--output', type=str, help='Path to output directory', default=None)
+
+parser.add_argument('--name', type=str, help='Name of the run', default=None)
+
+args = parser.parse_args()
+
+# -------- mlflow --------
+experiment_name = "dice_classifier"
+experiment = mlflow.get_experiment_by_name(experiment_name)
+if not experiment:
+    experiment_id = mlflow.create_experiment(experiment_name)
+else:
+    experiment_id = experiment.experiment_id
+run_name = args.name if args.name else f"{datetime.datetime.now().strftime('%Y%d%m%H%M%S')}-dice-classifier"
+print("experiment_id:", experiment_id)
+print("run_name:", run_name)
+mlflow.autolog()
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+# -------- hyperparameters --------
+learning_rate = args.learning_rate
+batch_size = args.batch_size
+epochs = args.epochs
 
+# -------- load model --------
+model_filepath = Path(args.model).resolve()
+print("model exists:", model_filepath.exists())
+model_dice = torch.load(model_filepath)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# -------- dataset --------
+data_dirpath = Path(args.data).resolve()
+ds_train = DiceImageDataset(data_dirpath / 'train')
+ds_valid = DiceImageDataset(data_dirpath / 'valid')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+dataloader_train = DataLoader(ds_train, batch_size=batch_size, shuffle=True)
+dataloader_valid = DataLoader(ds_valid, batch_size=batch_size, shuffle=True)
+
+# -------- initialize the loss function --------
+loss_function = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(model_dice.parameters(), lr=learning_rate)
+
+# -------- init mlflow run --------
+with mlflow.start_run(experiment_id=experiment_id, run_name=run_name):
+    mlflow.log_param("learning_rate", learning_rate)
+    mlflow.log_param("batch_size", batch_size)
+    mlflow.log_param("epochs", epochs)
+    mlflow.log_param("model_filepath", model_filepath)
+    mlflow.log_param("data_dirpath", data_dirpath)
+
+    # -------- training loop --------
+    output_filepath = Path(args.output).resolve() if args.output else None
+    for t in range(epochs):
+        print(f"Epoch {t + 1}\n-------------------------------")
+        train_loop(dataloader_train, model_dice, loss_function, optimizer)
+        test_loop(dataloader_valid, model_dice, loss_function)
+        if output_filepath:
+            torch.save(model_dice, output_filepath)
+        print()
+
+    # -------- save model --------
+    if output_filepath:
+        torch.save(model_dice, output_filepath)
+
+# -------- finish --------
+print("Done!")
